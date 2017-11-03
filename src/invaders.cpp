84da246101f892d2d811c54e57a00f2052d9f84e
kaochen <kaochen2@gmail.com>
2017-11-03 23:33:54 +0100

    add a background to the life status bar

--- src/invaders.cpp
+++ src/invaders.cpp
@@ -46,10 +46,10 @@ void C_invaders::move(int direction,
 
 void C_invaders::renderLifeBar(int x_screen, int y_screen, SDL_Renderer *renderer)
 	{
+		//add a life status above the boat
 		int l = m_life / 2;
 		int red = 0, green = 200;
-
-		SDL_Rect r;
+		SDL_Rect r, b;
 		    r.x = x_screen - TILE_HALF_WIDTH/2;
 		    r.y = y_screen + 64;
 		    r.w = l;
@@ -58,10 +58,14 @@ void C_invaders::renderLifeBar(int x_screen, int y_screen, SDL_Renderer *rendere
 		    	red = 200;
 		    	green = 0 ;
 		    	}
+		    b.x = r.x - 1;
+		    b.y = r.y - 1;
+		    b.w = MAX_LIFE/2 + 2;
+		    b.h = r.h + 2;
 
-		    // Set render color to blue ( rect will be rendered in this color )
+		    SDL_SetRenderDrawColor( renderer, 0, 0, 0, 255 );
+		    SDL_RenderFillRect( renderer, &b );
 		    SDL_SetRenderDrawColor( renderer, red, green, 0, 255 );
-
 		    // Render rect
 		    SDL_RenderFillRect( renderer, &r );
 	}
