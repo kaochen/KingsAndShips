a2a6ef2bfcde156836f182033d30969f4820ee1d
kaochen <kaochen2@gmail.com>
2017-11-04 16:38:23 +0100

    add a file to store time functions

---
+++ src/time.h
@@ -0,0 +1,20 @@
+#ifndef TIME_H
+#define TIME_H
+
+#include <iostream>
+#include <SDL2/SDL.h>
+
+#include "game.h"
+#include "settings.h"
+
+class C_Time: public C_Game
+{
+	public:
+
+	protected:
+
+	private:
+};
+
+
+#endif
