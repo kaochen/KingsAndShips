a386cc7762cb4006a6c709112d12f2b76bc6747e
kaochen <kaochen2@gmail.com>
2017-11-19 11:21:34 +0100

    add layers to the map and setup tilset offset

---
+++ data/levels/Ground_01.tsx
@@ -0,0 +1,6 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Ground_01" tilewidth="128" tileheight="128" tilecount="64" columns="8">
+ <tileoffset x="-14" y="11"/>
+ <grid orientation="orthogonal" width="8" height="1"/>
+ <image source="../img/original/t_ground_01.png" width="1024" height="1024"/>
+</tileset>
--- data/levels/boat_01.tsx
+++ data/levels/boat_01.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="boat_01" tilewidth="128" tileheight="128" tilecount="56" columns="8">
- <image source="../img/original/t_boat_01.png" width="1024" height="1008"/>
+<tileset name="boat_01" tilewidth="128" tileheight="128" tilecount="64" columns="8">
+ <tileoffset x="-14" y="11"/>
+ <grid orientation="orthogonal" width="8" height="8"/>
+ <image source="../img/original/t_boat_01.png" width="1024" height="1024"/>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="100"/>
+   <frame tileid="16" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="100"/>
+   <frame tileid="17" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="100"/>
+   <frame tileid="18" duration="100"/>
+  </animation>
+ </tile>
 </tileset>
