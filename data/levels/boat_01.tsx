8284716c95a8c62e7915616db60f6fd9030f1a02
kaochen <kaochen2@gmail.com>
2017-11-19 08:49:19 +0100

    start using Tiled and tileset

---
+++ data/levels/boat_01.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="boat_01" tilewidth="128" tileheight="128" tilecount="56" columns="8">
+ <image source="../img/original/t_boat_01.png" width="1024" height="1008"/>
+</tileset>
