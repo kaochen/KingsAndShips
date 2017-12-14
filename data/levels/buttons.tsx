cab645278a85d7346ce42d5139041856064e5f0f
kaochen <kaochen2@gmail.com>
2017-12-14 16:25:21 +0100

    change image for the addMillButton

--- data/levels/buttons.tsx
+++ data/levels/buttons.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="1" columns="1">
- <image source="../img/original/t_buttons.png" width="64" height="64"/>
+<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="2" columns="2">
+ <image source="../img/original/t_buttons.png" width="128" height="64"/>
  <tile id="0" type="AddTowerOut"/>
+ <tile id="1" type="AddMillOut"/>
 </tileset>
