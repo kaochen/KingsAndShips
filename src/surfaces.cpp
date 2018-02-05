3659b21eb167311e5d8dec0ebfaa8bff08d157e8
kaochen <kaochen2@gmail.com>
2018-02-05 07:53:07 +0100

    dump CrossBow image

--- src/surfaces.cpp
+++ src/surfaces.cpp
@@ -174,11 +174,10 @@ map<string, C_Texture*>  C_TextureList::getTextMap(){
 
 
 void C_TextureList::loadTexturesIntoMap(){
-	int size = 10;
-	string file[size] = {"boat_01_Dead.png",
-	"CrossBow_01.png","Arrow01_South.png","Arrow01_East.png","Arrow01_North.png",
-	"Arrow01_West.png","Arrow01_NorthEast.png","Arrow01_NorthWest.png",
-	"Arrow01_SouthEast.png","Arrow01_SouthWest.png"};
+	int size = 9;
+	string file[size] = {"boat_01_Dead.png","Arrow01_South.png",
+	"Arrow01_East.png","Arrow01_North.png",	"Arrow01_West.png",
+	"Arrow01_NorthEast.png","Arrow01_NorthWest.png","Arrow01_SouthEast.png","Arrow01_SouthWest.png"};
 
 
 	for (int i = 0; i < size; i++){
