5abb3f1f119d678c5df23a3cf14a1d432b25aa7a
kaochen <kaochen2@gmail.com>
2018-04-15 17:07:16 +0200

    allow town to shoot boats

--- src/town.cpp
+++ src/town.cpp
@@ -6,15 +6,18 @@ using namespace std;
 C_Town::C_Town( int x_grid,
                  int y_grid):C_Shooter("town", x_grid, y_grid, 1)
 {
-
+	m_weapon = new C_Weapon("ARCHER",10,0,500,2);
 }
 
 void C_Town::play(){
-	//this->shoot("boat");
+	this->shoot("boat");
 }
 
 void C_Town::render(S_Coord screen){
 	C_GameUnits::render(screen);
 	renderLifeBar(screen.x, screen.y);
+	if (m_weapon->getShooting()){
+		m_weapon->render();
+	}
 
 }
