b733b2def185182f5a7335db2be86fb9d349518d
kaochen <kaochen2@gmail.com>
2018-04-29 12:41:44 +0200

    fix forgotten comment marks on the licence notices :)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+/*
 This file is part of KingsAndShips.
 
 This program is free software: you can redistribute it and/or modify
@@ -12,6 +13,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 
 #include <iostream>
