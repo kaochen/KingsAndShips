68a59dac664aa8c18ba89bc5624648ae59fa8cbd
kaochen <kaochen2@gmail.com>
2018-05-09 13:05:34 +0200

    start an animation for the tree

--- data/levels/Trees_00.tsx
+++ data/levels/Trees_00.tsx
@@ -1,7 +1,21 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="trees" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+<tileset name="trees" tilewidth="128" tileheight="128" tilecount="6" columns="6">
  <tileoffset x="-14" y="11"/>
  <grid orientation="isometric" width="128" height="128"/>
- <image source="../img/original/t_trees_01.png" width="128" height="128"/>
- <tile id="0" type="01"/>
+ <image source="../img/original/t_trees_01.png" width="768" height="128"/>
+ <tile id="0" type="01_00">
+  <animation>
+   <frame tileid="0" duration="100"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="100"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="100"/>
+   <frame tileid="5" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="1" type="01_02"/>
+ <tile id="2" type="01_03"/>
+ <tile id="3" type="01_04"/>
+ <tile id="4" type="01_05"/>
+ <tile id="5" type="01_06"/>
 </tileset>
