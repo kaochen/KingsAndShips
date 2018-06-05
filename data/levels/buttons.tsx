e699ee0c8470bb3be6fe5c51e5b6c2f7f2bfc1e6
kaochen <kaochen2@gmail.com>
2018-06-05 10:33:24 +0200

    update buttons tileset

--- data/levels/buttons.tsx
+++ data/levels/buttons.tsx
@@ -1,7 +1,10 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="2" columns="2">
- <image source="../img/original/t_buttons.png" width="192" height="64"/>
+<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="6" columns="6">
+ <image source="../img/original/t_buttons.png" width="384" height="64"/>
  <tile id="0" type="AddTowerOut"/>
- <tile id="1" type="AddTurbineOut"/>
- <tile id="2" type="lys"/>
+ <tile id="1" type="AddTowerOver"/>
+ <tile id="2" type="AddTowerOff"/>
+ <tile id="3" type="AddTurbineOut"/>
+ <tile id="4" type="AddTurbineOver"/>
+ <tile id="5" type="AddTurbineOff"/>
 </tileset>
