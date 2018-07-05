ca0469ab5859e7da84ff78cb3ced4147238f368f
kaochen <kaochen2@gmail.com>
2018-07-05 17:46:19 +0200

    change offsets

--- data/img/town_01.tsx
+++ data/img/town_01.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="town_01" tilewidth="256" tileheight="256" tilecount="4" columns="1">
+<tileset name="town_01" tilewidth="256" tileheight="256" tilecount="1" columns="1">
+ <tileoffset x="-70" y="55"/>
  <grid orientation="orthogonal" width="8" height="8"/>
  <image source="original/Town_01.png" width="256" height="256"/>
  <tile id="0" type="EE_0"/>
