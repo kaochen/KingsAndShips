a1aca1cc30b005e0d41a99dec1baef55bce18046
kaochen <kaochen2@gmail.com>
2018-08-18 19:12:33 +0200

    add a barricade with fire on it

---
+++ data/img/barricade_01.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="barricade" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+ <tileoffset x="-14" y="11"/>
+ <image source="original/t_barricade_01.png" width="128" height="128"/>
+</tileset>
