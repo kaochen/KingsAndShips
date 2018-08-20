cae9fa4cb00831525d5c87dfe5c4b0cddeedbb35
kaochen <kaochen2@gmail.com>
2018-08-20 19:59:29 +0200

    draw a basic barricade button

--- data/img/buttons.tsx
+++ data/img/buttons.tsx
@@ -1,10 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="6" columns="6">
- <image source="original/t_buttons.png" width="384" height="64"/>
+<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="12" columns="6">
+ <image source="original/t_buttons.png" width="384" height="128"/>
  <tile id="0" type="AddTower_Active"/>
  <tile id="1" type="AddTower_Hover"/>
  <tile id="2" type="AddTower_Disabled"/>
  <tile id="3" type="AddTurbine_Active"/>
  <tile id="4" type="AddTurbine_Hover"/>
  <tile id="5" type="AddTurbine_Disabled"/>
+ <tile id="6" type="AddBarricade_Active"/>
+ <tile id="7" type="AddBarricade_Hover"/>
+ <tile id="8" type="AddBarricade_Disabled"/>
+ <tile id="9" type="AddSpare_Active"/>
+ <tile id="10" type="AddSpare_Hover"/>
+ <tile id="11" type="AddSpare_Disabled"/>
 </tileset>
