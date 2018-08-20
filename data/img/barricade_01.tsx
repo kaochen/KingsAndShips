24c09eec66eb0c93ec63338b0f99993e937ceac3
kaochen <kaochen2@gmail.com>
2018-08-20 19:16:05 +0200

    load barricade images  at start

--- data/img/barricade_01.tsx
+++ data/img/barricade_01.tsx
@@ -1,5 +1,16 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="barricade" tilewidth="128" tileheight="128" tilecount="1" columns="1">
+<tileset name="barricade_1" tilewidth="128" tileheight="128" tilecount="16" columns="8">
  <tileoffset x="-14" y="11"/>
- <image source="original/t_barricade_01.png" width="128" height="128"/>
+ <image source="original/t_barricade_01.png" width="1024" height="256"/>
+ <tile id="0" type="A_0"/>
+ <tile id="1" type="A_1"/>
+ <tile id="2" type="A_2"/>
+ <tile id="3" type="A_3"/>
+ <tile id="4" type="A_4"/>
+ <tile id="5" type="A_5"/>
+ <tile id="6" type="A_6"/>
+ <tile id="7" type="A_7"/>
+ <tile id="8" type="A_8"/>
+ <tile id="9" type="A_9"/>
+ <tile id="10" type="A_10"/>
 </tileset>
