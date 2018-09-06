a591868a5d18fb3e8964c98d34ed1aea41327f61
kaochen <kaochen2@gmail.com>
2018-09-06 10:48:35 +0200

    update buttons design, add an empty one at first if needed

--- data/img/buttons.tsx
+++ data/img/buttons.tsx
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="12" columns="6">
  <image source="original/t_buttons.png" width="384" height="128"/>
- <tile id="0" type="AddTower_Active"/>
- <tile id="1" type="AddTower_Hover"/>
- <tile id="2" type="AddTower_Disabled"/>
- <tile id="3" type="AddTurbine_Active"/>
- <tile id="4" type="AddTurbine_Hover"/>
- <tile id="5" type="AddTurbine_Disabled"/>
- <tile id="6" type="AddBarricade_Active"/>
- <tile id="7" type="AddBarricade_Hover"/>
- <tile id="8" type="AddBarricade_Disabled"/>
- <tile id="9" type="AddSpare_Active"/>
- <tile id="10" type="AddSpare_Hover"/>
- <tile id="11" type="AddSpare_Disabled"/>
+ <tile id="0" type="AddSpare_Active"/>
+ <tile id="1" type="AddSpare_Hover"/>
+ <tile id="2" type="AddSpare_Disabled"/>
+ <tile id="3" type="AddTower_Active"/>
+ <tile id="4" type="AddTower_Hover"/>
+ <tile id="5" type="AddTower_Disabled"/>
+ <tile id="6" type="AddTurbine_Active"/>
+ <tile id="7" type="AddTurbine_Hover"/>
+ <tile id="8" type="AddTurbine_Disabled"/>
+ <tile id="9" type="AddBarricade_Active"/>
+ <tile id="10" type="AddBarricade_Hover"/>
+ <tile id="11" type="AddBarricade_Disabled"/>
+
 </tileset>
