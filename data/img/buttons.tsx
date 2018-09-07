857d4ec96ff4f51e3d45024500a83dc519089ba9
kaochen <kaochen2@gmail.com>
2018-09-07 10:59:57 +0200

    add new buttons to the tileset : hamburger menu, play and pause

--- data/img/buttons.tsx
+++ data/img/buttons.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="12" columns="6">
- <image source="original/t_buttons.png" width="384" height="128"/>
+<tileset name="Buttons" tilewidth="64" tileheight="64" tilecount="30" columns="6">
+ <image source="original/t_buttons.png" width="384" height="320"/>
  <tile id="0" type="AddSpare_Active"/>
  <tile id="1" type="AddSpare_Hover"/>
  <tile id="2" type="AddSpare_Disabled"/>
@@ -13,5 +13,19 @@
  <tile id="9" type="AddBarricade_Active"/>
  <tile id="10" type="AddBarricade_Hover"/>
  <tile id="11" type="AddBarricade_Disabled"/>
-
+ <tile id="12" type="AddSpare2_Active"/>
+ <tile id="13" type="AddSpare2_Hover"/>
+ <tile id="14" type="AddSpare2_Disabled"/>
+ <tile id="15" type="MenuSpare_Active"/>
+ <tile id="16" type="MenuSpare_Hover"/>
+ <tile id="17" type="MenuSpare_Disabled"/>
+ <tile id="18" type="Menu_Active"/>
+ <tile id="19" type="Menu_Hover"/>
+ <tile id="20" type="Menu_Disabled"/>
+ <tile id="21" type="Play_Active"/>
+ <tile id="22" type="Play_Hover"/>
+ <tile id="23" type="Play_Disabled"/>
+ <tile id="24" type="Pause_Active"/>
+ <tile id="25" type="Pause_Hover"/>
+ <tile id="26" type="Pause_Disabled"/>
 </tileset>
