937ddfd3dfa202d2b1b54e23cf6a03903ce573fe
kaochen <kaochen2@gmail.com>
2018-09-09 21:35:37 +0200

    design a gold icon and an animation

---
+++ data/img/gold.tsx
@@ -0,0 +1,31 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="gold" tilewidth="128" tileheight="128" tilecount="32" columns="8">
+ <image source="original/t_gold.png" width="1024" height="512"/>
+ <tile id="0" type="pile"/>
+ <tile id="1" type="still"/>
+ <tile id="2" type="anim_1"/>
+ <tile id="3" type="anim_2"/>
+ <tile id="4" type="anim_3"/>
+ <tile id="5" type="anim_4"/>
+ <tile id="6" type="anim_5"/>
+ <tile id="7" type="anim_6"/>
+ <tile id="8" type="anim_7"/>
+ <tile id="9" type="anim_8"/>
+ <tile id="10" type="anim_9"/>
+ <tile id="11" type="anim_10"/>
+ <tile id="12" type="anim_11"/>
+ <tile id="13" type="anim_12"/>
+ <tile id="14" type="anim_13"/>
+ <tile id="15" type="anim_14"/>
+ <tile id="16" type="anim_15"/>
+ <tile id="17" type="anim_16"/>
+ <tile id="18" type="anim_17"/>
+ <tile id="19" type="anim_18"/>
+ <tile id="20" type="anim_19"/>
+ <tile id="21" type="anim_20"/>
+ <tile id="22" type="anim_21"/>
+ <tile id="23" type="anim_22"/>
+ <tile id="24" type="anim_23"/>
+ <tile id="25" type="anim_24"/>
+ <tile id="26" type="anim_25"/>
+</tileset>
