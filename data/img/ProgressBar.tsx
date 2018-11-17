dca58dae84191993851fb2db03e198d4400b172c
kaochen <kaochen2@gmail.com>
2018-11-17 22:19:21 +0100

    create a new tileset with a new progress bar design

---
+++ data/img/ProgressBar.tsx
@@ -0,0 +1,13 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.0" name="ProgressBar" tilewidth="48" tileheight="48" tilecount="9" columns="3">
+ <image source="original/t_progress.png" width="144" height="144"/>
+ <tile id="0" type="Left_Green"/>
+ <tile id="1" type="Center_Green"/>
+ <tile id="2" type="Right_Green"/>
+ <tile id="3" type="Left_Red"/>
+ <tile id="4" type="Center_Red"/>
+ <tile id="5" type="Right_Red"/>
+ <tile id="6" type="Left_Blue"/>
+ <tile id="7" type="Center_Blue"/>
+ <tile id="8" type="Right_Blue"/>
+</tileset>
