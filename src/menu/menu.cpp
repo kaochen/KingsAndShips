7530a26e384dda9dd3ed6a658e78db34f11f91ac
kaochen <kaochen2@gmail.com>
2018-11-24 09:50:05 +0100

    move progressbar in to a better place

--- src/menu/menu.cpp
+++ src/menu/menu.cpp
@@ -112,7 +112,7 @@ void C_Menu::updateDefenderStatus(){
 
     //progress bar value
     if(m_menuItemsList["playerlife"] == nullptr){
-        m_menuItemsList["playerlife"] = new C_GP_Status("playerlife",x - 192,40, GREEN, RED);
+        m_menuItemsList["playerlife"] = new C_GP_Status("playerlife",x - 200,40, GREEN, RED);
     }
     string text = "Life: " + nbrToString(playerLife);
     if(m_menuItemsList["playerlife"] != nullptr){
@@ -131,7 +131,7 @@ void C_Menu::updateAttackerStatus(){
         		m_menuItemsList["Characters_lion"] = new C_MenuItem("Characters_lion",20,30);
         }
         if(m_menuItemsList["boatLife"] == nullptr){
-		    m_menuItemsList["boatLife"] = new C_GP_Status("boatLife",50,40, GREEN,BLUE);
+		    m_menuItemsList["boatLife"] = new C_GP_Status("boatLife",60,40, GREEN,BLUE);
 		}
 		if(m_menuItemsList["boatLife"] != nullptr){
     		string text = "Wave " + to_string(m_total_waves - m_current_wave +1) + "/" + to_string(m_total_waves);
@@ -145,11 +145,11 @@ void C_Menu::updateWalletStatus(){
 	int x = settings.getWindowWidth();
 	C_Wallet& wallet=C_Wallet::Instances();
     if(m_menuItemsList["gold_pile"] == nullptr){
-	    m_menuItemsList["gold_pile"] = new C_MenuItem("gold_pile",x - 236,58);
+	    m_menuItemsList["gold_pile"] = new C_MenuItem("gold_pile",x - 218,40);
 	}
 	//progress bar
     if(m_menuItemsList["walletBar"]== nullptr){
-		m_menuItemsList["walletBar"] = new C_GP_Status("walletBar",x - 192,100, GREEN, BLUE);
+		m_menuItemsList["walletBar"] = new C_GP_Status("walletBar",x - 168,80, GREEN, BLUE);
 		}
 	 if(m_menuItemsList["walletBar"] != nullptr){
     	string text = nbrToString(wallet.getBalance());
