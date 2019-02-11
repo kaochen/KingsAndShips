41c99e5aa6e1162d7901bae21319d6d0f0b97df1
kaochen <kaochen2@gmail.com>
2019-02-11 22:21:50 +0100

    design a new background for cards

---
+++ data/img/Parchments.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Parchment" tilewidth="256" tileheight="256" tilecount="3" columns="3">
+ <image source="original/t_parchment_01.png" width="768" height="256"/>
+ <tile id="0" type="Parchment"/>
+ <tile id="1" type="Card_Active"/>
+ <tile id="2" type="Card_Hover"/>
+</tileset>
