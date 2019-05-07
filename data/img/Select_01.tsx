a94cbdb0f769b927b1b57824af35c5e1ac787117
kaochen <kaochen2@gmail.com>
2019-05-07 22:06:23 +0200

    add a new image to indicate area zones when the player drag and drop a tower

---
+++ data/img/Select_01.tsx
@@ -0,0 +1,36 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="Select" tilewidth="128" tileheight="128" tilecount="32" columns="8">
+ <image source="original/t_select_01.png" width="1024" height="512"/>
+ <tile id="0" type="Green_01"/>
+ <tile id="1" type="Green_02"/>
+ <tile id="2" type="Green_03"/>
+ <tile id="3" type="Green_04"/>
+ <tile id="4" type="Green_05"/>
+ <tile id="5" type="Green_06"/>
+ <tile id="6" type="Green_07"/>
+ <tile id="7" type="Green_08"/>
+ <tile id="8" type="Red_01"/>
+ <tile id="9" type="Red_02"/>
+ <tile id="10" type="Red_03"/>
+ <tile id="11" type="Red_04"/>
+ <tile id="12" type="Red_05"/>
+ <tile id="13" type="Red_06"/>
+ <tile id="14" type="Red_07"/>
+ <tile id="15" type="Red_08"/>
+ <tile id="16" type="Corner_Green_EE"/>
+ <tile id="17" type="Corner_Green_SE"/>
+ <tile id="18" type="Corner_Green_SS"/>
+ <tile id="19" type="Corner_Green_SW"/>
+ <tile id="20" type="Corner_Green_WW"/>
+ <tile id="21" type="Corner_Green_NW"/>
+ <tile id="22" type="Corner_Green_NN"/>
+ <tile id="23" type="Corner_Green_NE"/>
+ <tile id="24" type="Corner_Red_NE"/>
+ <tile id="25" type="Corner_Red_SE"/>
+ <tile id="26" type="Corner_Red_SS"/>
+ <tile id="27" type="Corner_Red_SW"/>
+ <tile id="28" type="Corner_Red_WW"/>
+ <tile id="29" type="Corner_Red_NW"/>
+ <tile id="30" type="Corner_Red_NN"/>
+ <tile id="31" type="Corner_Red_NE"/>
+</tileset>
