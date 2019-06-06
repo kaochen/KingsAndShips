7113ff00caa54e1ba05b87079bf2284b0f34297a
kaochen <kaochen2@gmail.com>
2019-06-06 10:44:51 +0200

    add a tileset with basic design clouds

---
+++ data/img/Clouds_00.tsx
@@ -0,0 +1,9 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="clouds" tilewidth="128" tileheight="128" tilecount="3" columns="3">
+ <tileoffset x="-14" y="11"/>
+ <grid orientation="isometric" width="128" height="128"/>
+ <image source="original/t_clouds_01.png" width="384" height="128"/>
+ <tile id="0" type="01"/>
+ <tile id="1" type="02"/>
+ <tile id="2" type="03"/>
+</tileset>
