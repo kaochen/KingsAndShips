5c205631b2961c0d6966b0c7d44b63c562f94abc
kaochen <kaochen2@gmail.com>
2019-07-07 14:36:03 +0200

    draw clouds for borders and corners

--- data/img/Clouds_00.tsx
+++ data/img/Clouds_00.tsx
@@ -1,11 +1,19 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="clouds" tilewidth="128" tileheight="128" tilecount="5" columns="5">
+<tileset version="1.2" tiledversion="1.2.1" name="clouds" tilewidth="128" tileheight="128" tilecount="18" columns="6">
  <tileoffset x="-14" y="11"/>
  <grid orientation="isometric" width="128" height="128"/>
- <image source="original/t_clouds_01.png" width="640" height="128"/>
+ <image source="original/t_clouds_01.png" width="768" height="384"/>
  <tile id="0" type="shadow"/>
  <tile id="1" type="01"/>
  <tile id="2" type="02"/>
  <tile id="3" type="03"/>
  <tile id="4" type="04"/>
+ <tile id="5" type="SW"/>
+ <tile id="6" type="SE"/>
+ <tile id="7" type="NE"/>
+ <tile id="8" type="NW"/>
+ <tile id="9" type="SS"/>
+ <tile id="10" type="WW"/>
+ <tile id="11" type="NN"/>
+ <tile id="12" type="EE"/>
 </tileset>
