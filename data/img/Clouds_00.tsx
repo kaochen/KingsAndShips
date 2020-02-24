69a5d78215b0cfe0fcd503c53bd6b35e2d4814cf
Kao_Chen <kaochen2@gmail.com>
2020-02-24 10:36:54 +0100

    render clouds to 256x256px

--- data/img/Clouds_00.tsx
+++ data/img/Clouds_00.tsx
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="clouds" tilewidth="128" tileheight="64" tilecount="5" columns="5">
+<tileset version="1.2" tiledversion="1.3.1" name="clouds" tilewidth="256" tileheight="128" tilecount="10" columns="5">
  <tileoffset x="-14" y="11"/>
- <grid orientation="isometric" width="128" height="64"/>
- <image source="original/t_clouds_01.png" width="640" height="128" zoom="yes"/>
+ <grid orientation="isometric" width="5" height="2"/>
+ <image source="original/t_clouds_01.png" width="1280" height="256"/>
  <tile id="0" type="Cloud_00"/>
  <tile id="1" type="Cloud_01"/>
  <tile id="2" type="Cloud_02"/>
