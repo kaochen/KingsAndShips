48acc28935bbf4961740851fe0153d6fa60e6a28
Kao_Chen <kaochen2@gmail.com>
2020-03-03 22:06:01 +0100

    recreate rocks at 256x256px

--- data/img/Rocks_00.tsx
+++ data/img/Rocks_00.tsx
@@ -1,8 +1,8 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="rocks" tilewidth="128" tileheight="128" tilecount="3" columns="3">
- <tileoffset x="-14" y="11"/>
- <grid orientation="isometric" width="128" height="128"/>
- <image source="original/t_rocks_01.png" width="384" height="128"/>
+<tileset name="rocks" tilewidth="256" tileheight="256" tilecount="3" columns="3">
+ <tileoffset x="-32" y="32"/>
+ <grid orientation="isometric" width="256" height="256"/>
+ <image source="original/t_rocks_01.png" width="768" height="256"/>
  <tile id="0" type="01"/>
  <tile id="1" type="02"/>
  <tile id="2" type="03"/>
