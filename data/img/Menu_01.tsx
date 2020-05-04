4642b3cf83ae50503c8b489d3d25436428bb6d9b
Kao_Chen <kaochen2@gmail.com>
2020-05-04 15:17:51 +0200

    upgrade menu background design

--- data/img/Menu_01.tsx
+++ data/img/Menu_01.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="Menu_01" tilewidth="1920" tileheight="400" tilecount="1" columns="1">
- <image source="original/menu_background.png" width="1920" height="400"/>
+<tileset version="1.2" tiledversion="1.2.1" name="Menu_01" tilewidth="900" tileheight="600" tilecount="1" columns="1">
+ <image source="original/menu_background.png" width="900" height="600"/>
  <tile id="0" type="background"/>
 </tileset>
