f45389deb491f58f81d092e303c61b82c26fac80
Kao_Chen <kaochen2@gmail.com>
2020-05-04 15:37:56 +0200

    simplify menu_buttons.png

---
+++ data/img/Menu_02.tsx
@@ -0,0 +1,8 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="Menu_01" tilewidth="450" tileheight="150" tilecount="8" columns="2">
+ <image source="original/menu_buttons.png" width="900" height="600"/>
+ <tile id="0" type="bannerLeft"/>
+ <tile id="1" type="bannerRight"/>
+ <tile id="2" type="flagOpen"/>
+ <tile id="4" type="flagClosed"/>
+</tileset>
