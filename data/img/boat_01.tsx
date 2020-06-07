d1f0c8ab049050ac33562a31d9b47a3cd169aba9
Kao_Chen <kaochen2@gmail.com>
2020-06-07 15:27:33 +0200

    create a sinking animation for boat_01

--- data/img/boat_01.tsx
+++ data/img/boat_01.tsx
@@ -12,7 +12,7 @@
   <property name="weapon.firerange" type="int" value="2"/>
   <property name="weapon.firerate" type="int" value="2000"/>
  </properties>
- <image source="original/t_boat_01.png" width="2048" height="2048"/>
+ <image source="original/t_boat_01-0.png" width="2048" height="2048"/>
  <tile id="0" type="A_EE_0"/>
  <tile id="1" type="A_EE_1"/>
  <tile id="2" type="A_EE_2"/>
